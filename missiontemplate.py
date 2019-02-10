35de2134d0d24f88336c072e201a0b1577a324df
Owen Fidler <fidler.owen@gmail.com>
2019-02-10 14:39:50 -0800

    here is the mission template

---
+++ missiontemplate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#import sys sys.path.append()
+
+from Robot import Robot
+myRobot = Robot()
+
+'''
+"myRobot.":
+moveUntilDistanceAway(distance, speed), 
+followLine(onLeft(left = True, Right = False),followDistance), 
+goToLine(color(B=0, W=100), range, speed),
+moveForwardRot(rotations, speed)
+moveForwardCm(rotations, speed, circ(16.5 on a standard wheel))
+moveLargeMotor()
+'''
